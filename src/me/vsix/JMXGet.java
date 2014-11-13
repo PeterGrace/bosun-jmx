6a56a510b697930e03316d100cdff8e573aad16e
Peter Grace <pete@stackexchange.com>
2014-11-13 16:21:16 -0500

    Add code, add ant build script

---
+++ src/me/vsix/JMXGet.java
@@ -0,0 +1,104 @@
+package me.vsix;
+
+import java.util.Set;
+import javax.management.MBeanServerConnection;
+import javax.management.ObjectName;
+import javax.management.ObjectInstance;
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
+import javax.management.openmbean.CompositeDataSupport;
+
+public class JMXGet {
+
+    /* For simplicity, we declare "throws Exception".
+       Real programs will usually want finer-grained exception handling. */
+    public static void main(String[] args) throws Exception {
+        JMXServiceURL url =
+            new JMXServiceURL("service:jmx:rmi:///jndi/rmi://:1099/jmxrmi");
+        JMXConnector jmxc = JMXConnectorFactory.connect(url, null);
+
+        MBeanServerConnection mbsc = jmxc.getMBeanServerConnection();
+
+	while(true)
+	{
+		getHeapMemoryUsage(mbsc);
+		getNonHeapMemoryUsage(mbsc);
+		getPoolMemoryUsage(mbsc);
+		sleep(1000);
+	}
+
+
+    }
+private static void getHeapMemoryUsage(MBeanServerConnection con) throws Exception
+{
+    long timestamp = System.currentTimeMillis() / 1000l;
+    ObjectName memoryMXBean=new ObjectName("java.lang:type=Memory");
+    CompositeDataSupport dataSenders = (CompositeDataSupport) con.getAttribute(memoryMXBean,"HeapMemoryUsage");
+    if (dataSenders != null)
+      {
+        Long commited = (Long) dataSenders.get("committed");
+        Long init = (Long) dataSenders.get("init");
+        Long max = (Long) dataSenders.get("max");
+        Long used = (Long) dataSenders.get("used");
+        Long percentage = ((used * 100) / max);
+        System.out.println("jmx.mem.heap.committed "+timestamp+" "+commited);
+        System.out.println("jmx.mem.heap.init "+timestamp+" "+init);
+        System.out.println("jmx.mem.heap.max "+timestamp+" "+max);
+        System.out.println("jmx.mem.heap.used "+timestamp+" "+used);
+        System.out.println("jmx.mem.heap.percent_used "+timestamp+" "+percentage);
+       }
+}
+private static void getNonHeapMemoryUsage(MBeanServerConnection con) throws Exception
+{
+    long timestamp = System.currentTimeMillis() / 1000l;
+    ObjectName memoryMXBean=new ObjectName("java.lang:type=Memory");
+    CompositeDataSupport dataSenders = (CompositeDataSupport) con.getAttribute(memoryMXBean,"NonHeapMemoryUsage");
+    if (dataSenders != null)
+      {
+        Long commited = (Long) dataSenders.get("committed");
+        Long init = (Long) dataSenders.get("init");
+        Long max = (Long) dataSenders.get("max");
+        Long used = (Long) dataSenders.get("used");
+        Long percentage = ((used * 100) / max);
+        System.out.println("jmx.mem.non_heap.committed "+timestamp+" "+commited);
+        System.out.println("jmx.mem.non_heap.init "+timestamp+" "+init);
+        System.out.println("jmx.mem.non_heap.max "+timestamp+" "+max);
+        System.out.println("jmx.mem.non_heap.used "+timestamp+" "+used);
+        System.out.println("jmx.mem.non_heap.percent_used "+timestamp+" "+percentage);
+       }
+}
+private static void getPoolMemoryUsage(MBeanServerConnection con) throws Exception
+{
+        ObjectName MemoryPoolMXBean=new ObjectName("java.lang:type=MemoryPool,name=*");
+        Set<ObjectInstance> MemoryPoolResult=con.queryMBeans(MemoryPoolMXBean,null);
+	for( ObjectInstance instance : MemoryPoolResult )
+	{
+	    String bosunname=instance.getObjectName().toString().split(",")[1].split("=")[1].replace(" ","_");
+	    long timestamp = System.currentTimeMillis() / 1000l;
+	    CompositeDataSupport dataSenders = (CompositeDataSupport) con.getAttribute(instance.getObjectName(),"Usage");
+
+        Long commited = (Long) dataSenders.get("committed");
+        Long init = (Long) dataSenders.get("init");
+        Long max = (Long) dataSenders.get("max");
+        Long used = (Long) dataSenders.get("used");
+        Long percentage = ((used * 100) / max);
+        System.out.println("jmx.mem.pool.committed "+timestamp+" "+commited+" pooltype="+bosunname);
+        System.out.println("jmx.mem.pool.init "+timestamp+" "+init+" pooltype="+bosunname);
+        System.out.println("jmx.mem.pool.max "+timestamp+" "+max+" pooltype="+bosunname);
+        System.out.println("jmx.mem.pool.used "+timestamp+" "+used+" pooltype="+bosunname);
+        System.out.println("jmx.mem.pool.percent_used "+timestamp+" "+percentage+" pooltype="+bosunname);
+
+	}
+
+
+}
+    private static void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
